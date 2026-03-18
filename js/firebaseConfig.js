cf51a9d66c820fadf07fb25cfa5fd264cee0a8c0
ZeroArtLord <armandoyanez829@gmail.com>
2026-03-18 15:06:53 -0400

    Update Firebase config keys

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,10 +2,10 @@
 // Nota: No usar el Admin SDK en el navegador.
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo_pmlSvXUbdDTfQwXEybmquMezusMuP4",
+    apiKey: "AIzaSyDo_pmISvXUbdDTfQwXEybmquMezusMuP4",
     authDomain: "smart-inventory-c296b.firebaseapp.com",
     projectId: "smart-inventory-c296b",
     storageBucket: "smart-inventory-c296b.firebasestorage.app",
-    messagingSenderId: "640420817972",
-    appId: "1:640420817972:web:56880b3c8f6f38a5bf255"
+    messagingSenderId: "640420819792",
+    appId: "1:640420819792:web:560800b3c8f6f38a5bf255"
 };
