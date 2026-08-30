a08f688268f9c273ff403cc646cd6d83d8e83a8a
ZeroArtLord <armandoyanez829@gmail.com>
2026-08-30 16:23:18 -0400

    V2: add offline application shell cache

---
+++ v2/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'smart-inventory-v2-shell-1';
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './css/app.css',
+  './manifest.webmanifest',
+  './src/ui/app.js',
+  './src/core/mathExpression.js',
+  './src/core/ids.js',
+  './src/core/catalog.js',
+  './src/core/movementTypes.js',
+  './src/catalog/catalogService.js',
+  './src/storage/database.js',
+  './src/sync/localQueue.js',
+  './src/inventory/stockEngine.js',
+  './src/inventory/movementService.js',
+  './src/intelligence/replenishmentEngine.js',
+  './src/documents/documentTypes.js',
+  './src/documents/documentService.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
