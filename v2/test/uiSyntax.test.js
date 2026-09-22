cff11a78afd8a5aaa80fb2115c7f19325712f351
ZeroArtLord <armandoyanez829@gmail.com>
2026-09-22 15:18:56 -0400

    test: include Phase B barcode UI syntax

--- v2/test/uiSyntax.test.js
+++ v2/test/uiSyntax.test.js
@@ -10,6 +10,7 @@ const files = [
   '../src/ui/countReconciliationBulkUi.js',
   '../src/ui/liveSupplyUi.js',
   '../src/ui/supplyChecklistUi.js',
+  '../src/ui/barcodeAssociationUi.js',
   '../src/ui/quickStockCorrectionUi.js',
   '../src/ui/quickStockCorrectionRefreshUi.js',
   '../src/ui/procurementWorkspaceV6Ui.js',
